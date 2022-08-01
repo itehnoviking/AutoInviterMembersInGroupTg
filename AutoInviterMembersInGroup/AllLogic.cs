c0da660983a35cd52f68c1e87c20cde349e6f004
Alex Gaudi <itehnoviking@gmail.com>
2022-08-02 01:51:46 +0300

    Add project files.

---
+++ AutoInviterMembersInGroup/AllLogic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TL;
+
+namespace AutoInviterMembersInGroup
+{
+    public class AllLogic
+    {
+        public IList<string> GetListWithIdAndHashMembers(string path)
+        {
+            var list = new List<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+
+                return list;
+            }
+        }
+
+        public IList<string> CreatedThirtyMembersFromListAndSavingBigListInFile(IList<string> list, string path)
+        {
+            List<string> thirtyMembers = new List<string>();
+
+            for (int i = 0; i < 30 ; i++)
+            {
+                thirtyMembers.Add(list[i]);
+                list.Remove(list[i]);
+            }
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                foreach (var member in list)
+                {
+                    writer.WriteLineAsync(member);
+                }
+            }
+
+            return thirtyMembers;
+        }
+
+        public IDictionary<long, long> TransformListInDictionary(IList<string> list)
+        {
+            IDictionary<long, long> result = new Dictionary<long, long>();
+
+            foreach (var member in list)
+            {
+                string key = null;
+                string value = null;
+
+                for (int i = 0; i < member.Length; i++)
+                {
+
+                    if (member[i] == ':')
+                    {
+                        for (int a = i+1; a < member.Length; a++)
+                        {
+                            value += member[a];
+                        }
+                        break;
+                    }
+
+                    key += member[i];
+                }
+
+                result.Add(Convert.ToInt64(key), Convert.ToInt64(value));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AutoInviterMembersInGroup/Program.cs
@@ -0,0 +1,44 @@
+﻿using AutoInviterMembersInGroup;
+using TL;
+
+
+var randomSecundForPause = new Random();
+var logicClass = new AllLogic();
+
+const string PATH = @"C:\Users\itehn\Desktop\IdAndAccesHashMembers.txt";
+
+using var client = new WTelegram.Client();
+var myClient = await client.LoginUserIfNeeded();
+Console.WriteLine($"We are logged-in as {myClient.username ?? myClient.first_name + " " + myClient.last_name} (id {myClient.id})");
+
+
+var thirtList = logicClass.CreatedThirtyMembersFromListAndSavingBigListInFile(logicClass.GetListWithIdAndHashMembers(PATH), PATH);
+
+var idAndAccesHashDictionary = logicClass.TransformListInDictionary(thirtList);
+
+
+var chats = await client.Messages_GetAllChats();
+var targetChat = chats.chats[11111111111];
+
+var counter = 0;
+
+foreach (var item in idAndAccesHashDictionary)
+{
+    try
+    {
+        var user = new InputUser(item.Key, item.Value);
+        await client.AddChatUser(targetChat, user);
+        Thread.Sleep(randomSecundForPause.Next(100000, 150000));
+        counter++;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Exception");
+        Thread.Sleep(randomSecundForPause.Next(100000, 150000));
+    }
+}
+
+Console.WriteLine($"Added {counter} members in chat");
+
+
+
